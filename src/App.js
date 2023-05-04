ec3037b1f01cfc544f48cd46c5c28fab2937bd8f
Omaar01 <omar.fait.05@gmail.com>
2023-05-04 16:22:55 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react"
 function App() {
+  const[name, setName] = useState("Omar")
+  const[bio, setBio] = useState("moroccan")
+  const[profession, setProfession] = useState("pharma")
+  const[img, setImg] = useState("logo512.png")
+  const[check, setcheck] = useState(false)
+  function handlebouton(){ 
+    setcheck(!check)
+  }
+  if (check){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={handlebouton}>clickme</button>
+
+      <h1>Hello my {name} i am {bio} i work in a {profession} this is my image </h1>
+      <img src={img}></img>   
     </div>
   );
+} 
+else {
+  return(
+    <button onClick={handlebouton}>clickme</button>
+
+  )
 }
+  }
+
 
 export default App;
